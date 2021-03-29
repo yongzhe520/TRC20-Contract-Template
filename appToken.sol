323be22b712a6c0436686e35c14832abd8562ad2
yongzhe520 <35864018+yongzhe520@users.noreply.github.com>
2021-03-29 17:50:24 +0800

    Create appToken.sol

---
+++ appToken.sol
@@ -0,0 +1,22 @@
+// 0.5.1-c8a2
+//启用优化
+语用固结度^ 0。5。0 ;
+
+导入 “ ./ERC20.sol”；
+导入 “ ./ERC20Detailed.sol”；
+
+/ **
+ * @title SimpleToken
+ * @dev非常简单的ERC20令牌示例，其中所有令牌都已预先分配给创建者。
+ *请注意，他们以后可以使用`transfer`和其他方式随意分配这些令牌
+ *`ERC20`功能。
+ * /
+合约代币为ERC20，ERC20Detailed {
+
+    / **
+     * @dev构造函数，可为msg.sender提供所有现有令牌。
+     * /
+    构造函数（）public  ERC20Detailed（“ Apple”，“ APP”，18）{
+        _mint（MSG。寄件人，1000000 *（10  **  uint256（小数（））））;
+    }
+}
